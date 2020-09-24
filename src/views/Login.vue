e2be4b5d70a56ab7aa79c8e8c41dfd161d21d358
MindaugasSak <m.sakenis@gmail.com>
2020-09-24 21:27:48 +0300

    sign in options added

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -28,12 +28,14 @@
 
       <b-button native-type="submit" :type="btnType">Login</b-button>
     </form>
+    <section id="firebaseui-auth-container"></section>
   </div>
 </template>
 
 <script>
-import firebase from "firebase/app";
-import "firebase/auth";
+import firebase from "firebase";
+const firebaseui = require("firebaseui");
+import "firebaseui/dist/firebaseui.css";
 import Notification from "../components/Notification";
 
 export default {
@@ -71,6 +73,21 @@ export default {
           }
         );
     },
+  },
+  mounted() {
+    let ui = firebaseui.auth.AuthUI.getInstance();
+    if (!ui) {
+      ui = new firebaseui.auth.AuthUI(firebase.auth());
+    }
+    var uiConfig = {
+      signInSuccessUrl: "/orders",
+      signInOptions: [
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      ],
+    };
+    ui.start("#firebaseui-auth-container", uiConfig);
   },
 };
 </script>
