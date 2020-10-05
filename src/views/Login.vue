6c954e9c53356646e5214dda474fca2e7b4ef633
MindaugasSak <m.sakenis@gmail.com>
2020-10-05 17:23:56 +0300

    minor bug fix

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,99 +1,100 @@
 <template>
-  <div class="login">
-    <Notification
-      v-if="isActive"
-      type="is-light is-danger"
-      :message="errorMessage"
-    />
+	<div class="login">
+		<Notification
+			v-if="isActive"
+			type="is-light is-danger"
+			:message="errorMessage"
+		/>
 
-    <form v-on:submit.prevent="login">
-      <b-field label="Email">
-        <b-input
-          type="email"
-          placeholder="e.g jonas@email.com"
-          maxlength="30"
-          v-model="email"
-        ></b-input>
-      </b-field>
+		<form v-on:submit.prevent="login">
+			<b-field label="Email">
+				<b-input
+					type="email"
+					placeholder="e.g jonas@email.com"
+					maxlength="30"
+					v-model="email"
+				></b-input>
+			</b-field>
 
-      <b-field label="Password">
-        <b-input
-          type="password"
-          v-model="password"
-          placeholder="Password"
-          maxlength="30"
-          password-reveal
-        ></b-input>
-      </b-field>
+			<b-field label="Password">
+				<b-input
+					type="password"
+					v-model="password"
+					placeholder="Password"
+					maxlength="30"
+					password-reveal
+				></b-input>
+			</b-field>
 
-      <b-button native-type="submit" :type="btnType">Login</b-button>
-    </form>
-    <section id="firebaseui-auth-container"></section>
-  </div>
+			<b-button native-type="submit" :type="btnType">Login</b-button>
+		</form>
+		<section id="firebaseui-auth-container"></section>
+	</div>
 </template>
 
 <script>
-import firebase from "firebase";
-const firebaseui = require("firebaseui");
-import "firebaseui/dist/firebaseui.css";
-import Notification from "../components/Notification";
+	import firebase from "firebase/app";
+	import "firebase/auth";
+	const firebaseui = require("firebaseui");
+	import "firebaseui/dist/firebaseui.css";
+	import Notification from "../components/Notification";
 
-export default {
-  name: "Login",
-  components: { Notification },
-  data() {
-    return {
-      email: "",
-      password: "",
-      isActive: false,
-      errorMessage: "",
-      btnType: "is-primary",
-    };
-  },
-  methods: {
-    login() {
-      this.btnType = "is-primary is-loading";
-      firebase
-        .auth()
-        .signInWithEmailAndPassword(this.email, this.password)
-        .then(
-          () => {
-            this.btnType = "is-primary";
-            this.$router.push("/orders");
-            this.$buefy.notification.open({
-              message: "You are logged in now",
-              position: "is-top",
-              type: "is-light is-success",
-            });
-          },
-          (error) => {
-            this.btnType = "is-primary";
-            this.isActive = true;
-            this.errorMessage = error.message;
-          }
-        );
-    },
-  },
-  mounted() {
-    let ui = firebaseui.auth.AuthUI.getInstance();
-    if (!ui) {
-      ui = new firebaseui.auth.AuthUI(firebase.auth());
-    }
-    var uiConfig = {
-      signInSuccessUrl: "/orders",
-      signInOptions: [
-        firebase.auth.EmailAuthProvider.PROVIDER_ID,
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      ],
-    };
-    ui.start("#firebaseui-auth-container", uiConfig);
-  },
-};
+	export default {
+		name: "Login",
+		components: { Notification },
+		data() {
+			return {
+				email: "",
+				password: "",
+				isActive: false,
+				errorMessage: "",
+				btnType: "is-primary",
+			};
+		},
+		methods: {
+			login() {
+				this.btnType = "is-primary is-loading";
+				firebase
+					.auth()
+					.signInWithEmailAndPassword(this.email, this.password)
+					.then(
+						() => {
+							this.btnType = "is-primary";
+							this.$router.push("/orders");
+							this.$buefy.notification.open({
+								message: "You are logged in now",
+								position: "is-top",
+								type: "is-light is-success",
+							});
+						},
+						(error) => {
+							this.btnType = "is-primary";
+							this.isActive = true;
+							this.errorMessage = error.message;
+						}
+					);
+			},
+		},
+		mounted() {
+			let ui = firebaseui.auth.AuthUI.getInstance();
+			if (!ui) {
+				ui = new firebaseui.auth.AuthUI(firebase.auth());
+			}
+			var uiConfig = {
+				signInSuccessUrl: "/orders",
+				signInOptions: [
+					firebase.auth.EmailAuthProvider.PROVIDER_ID,
+					firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+					firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+				],
+			};
+			ui.start("#firebaseui-auth-container", uiConfig);
+		},
+	};
 </script>
 
 <style scoped>
-.login {
-  max-width: 600px;
-}
+	.login {
+		max-width: 600px;
+	}
 </style>
